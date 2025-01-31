fd4bf51115b2102bff44fe79ace4764794055e17
IanD1013 <ian.tung1013@gmail.com>
2025-01-31 21:57:33 +1300

    Create first EFCore model

---
+++ Dometrain.EFCore.API/Data/MoviesContext.cs
@@ -0,0 +1,15 @@
+using Dometrain.EFCore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dometrain.EFCore.API.Data;
+
+public class MoviesContext : DbContext
+{
+    public DbSet<Movie> Movies => Set<Movie>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=MoviesDB;User ID=sa;Password=MySaPassword123;TrustServerCertificate=True");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
