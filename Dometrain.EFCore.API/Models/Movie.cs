3d5848ab3e106f64fad8d6b8db8f9c75fa3a373e
IanD1013 <ian.tung1013@gmail.com>
2025-02-03 10:35:40 +1300

    Introduce data annotations for our model

--- Dometrain.EFCore.API/Models/Movie.cs
+++ Dometrain.EFCore.API/Models/Movie.cs
@@ -1,10 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Dometrain.EFCore.API.Models;
 
+[Table("Pictures")]
 public class Movie
 {
+    [Key]
     public int Id { get; set; }
+    [MaxLength(128)]
+    [Column(TypeName = "varchar")]
+    [Required]
     public string? Title { get; set; }    
+    [Column(TypeName = "date")]
     public DateTime ReleaseDate { get; set; }
+    [Column("Plot", TypeName = "varchar(max)")]
     public string? Synopsis { get; set; }
 }
 
