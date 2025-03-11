ad8c901752564e9dfe7263c67a63fd5aee447aa2
IanD1013 <ian.tung1013@gmail.com>
2025-03-12 12:35:14 +1300

    Generate values in the model

--- Dometrain.EFCore.API/Models/Genre.cs
+++ Dometrain.EFCore.API/Models/Genre.cs
@@ -6,6 +6,8 @@ public class Genre
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    [JsonIgnore]
+    public DateTime CreatedDate { get; set; }
 
     [JsonIgnore]
     public ICollection<Movie> Movies { get; set; } = new HashSet<Movie>();
