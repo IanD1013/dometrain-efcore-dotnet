ea33561d3ef38d2e231484afed0a3b241b7d6b5e
IanD1013 <ian.tung1013@gmail.com>
2025-03-14 10:51:53 +1300

    Testing - repository pattern

--- Dometrain.EFCore.API/Program.cs
+++ Dometrain.EFCore.API/Program.cs
@@ -1,5 +1,6 @@
 using System.Text.Json.Serialization;
 using Dometrain.EFCore.API.Data;
+using Dometrain.EfCore.API.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,7 @@ builder.Services.AddControllers()
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IGenreRepository, GenreRepository>();
 
 // Add a DbContext here
 builder.Services.AddDbContext<MoviesContext>();
